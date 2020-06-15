73a4e76d2e5686658db1025ac29f5a925a0ed5b7
Ramon Gimenes <ramongimee@hotmail.com>
2020-06-15 12:17:54 -0300

    Implementando o logout com tokens

--- src/usuarios/middlewares-autenticacao.js
+++ src/usuarios/middlewares-autenticacao.js
@@ -35,6 +35,12 @@ module.exports = {
                     return res.status(401)
                  }
 
+                 if (erro && erro.name === 'TokenExpiredError') {
+                     return res
+                        .status(401)
+                        .json({erro: erro.message , expiradoEm: erro.expiredAt})
+                 }
+
                  if (erro) {
                      return res.status(500).json({ erro: erro.message });
                  }
@@ -43,6 +49,7 @@ module.exports = {
                      return res.status(401).json();
                  }
 
+                req.token = info.token;
                 req.user = usuario;
                 return next();
             }
